82bccee2542f25bfc22f7178a5b1a8177edc5a68
AZelada <azelada@activeit.cl>
2024-08-08 16:59:52 -0400

    docker build and push

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,7 +1,17 @@
-
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
 
-const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
+const testCdkStack = new AppCdkStack(app, 'test', {});
+
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
+  ecrRepository: ecrCdkStack.repository,
+});
+
+  
--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -4,14 +4,23 @@ import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository,
+}
 
 
 export class PipelineCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
     super(scope, id, props);
     const sourceOutput = new codepipeline.Artifact();
     const buildOutput = new codepipeline.Artifact();
     const unitTestOutput = new codepipeline.Artifact();
+    const dockerBuildOutput = new codepipeline.Artifact();
+
     const githubSecret = secretsmanager.Secret.fromSecretNameV2(this, 'GitHubToken', 'github');
 
 const pipeline = new codepipeline.Pipeline(this,'Pipeline',{
@@ -53,6 +62,43 @@ const sourceAction = new codepipelineActions.GitHubSourceAction({
         outputs: [unitTestOutput],
       });
 
+      const dockerBuild = new codebuild.PipelineProject(this, 'DockerBuild', {
+        environmentVariables: {
+          IMAGE_TAG: { value: 'latest' },
+          IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+          AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+        },
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+          privileged: true,
+          computeType: codebuild.ComputeType.LARGE,
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+      });
+
+      const dockerBuildRolePolicy = new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: ['*'],
+        actions: [
+          'ecr:GetAuthorizationToken',
+          'ecr:BatchCheckLayerAvailability',
+          'ecr:GetDownloadUrlForLayer',
+          'ecr:GetRepositoryPolicy',
+          'ecr:DescribeRepositories',
+          'ecr:ListImages',
+          'ecr:DescribeImages',
+          'ecr:BatchGetImage',
+          'ecr:InitiateLayerUpload',
+          'ecr:UploadLayerPart',
+          'ecr:CompleteLayerUpload',
+          'ecr:PutImage',
+        ],
+      });
+  
+      dockerBuild.addToRolePolicy(dockerBuildRolePolicy);
+  
+  
+
 pipeline.addStage({
     stageName: 'Source',
     actions:[sourceAction]
@@ -68,6 +114,19 @@ pipeline.addStage({
   stageName: 'Code-Quality-Testing',
   actions: [unitTest],
 });
+
+pipeline.addStage({
+  stageName: 'Docker-Push-ECR',
+  actions: [
+    new codepipelineActions.CodeBuildAction({
+      actionName: 'Docker-Build',
+      project: dockerBuild,
+      input: sourceOutput,
+      outputs: [dockerBuildOutput],
+    }),
+  ],
+});
+
 
 
 
