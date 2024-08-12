1219e6a8331aabf8630c1f90c5b5a9cdb01d510a
AZelada <azelada@activeit.cl>
2024-08-12 14:35:11 -0400

    despliegue continuo

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -8,13 +8,16 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 const app = new cdk.App();
 
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
+const testCdkStack = new AppCdkStack(app, 'test', {
+  ecrRepository: ecrCdkStack.repository,
+});
+
 const s3BucketStack = new cdk.Stack(app, 's3-bucket-stack');
 const s3Bucket = new s3.Bucket(s3BucketStack, 'DockerImageBucket', {
     removalPolicy: cdk.RemovalPolicy.DESTROY, // Adjust as needed
     autoDeleteObjects: true, // Adjust as needed
 });
-
-const testCdkStack = new AppCdkStack(app, 'test', {});
 
 const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
   ecrRepository: ecrCdkStack.repository,
