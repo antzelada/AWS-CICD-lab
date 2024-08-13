4ce656adfef82a61333d6aecfd09026e6efcb6e4
AZelada <azelada@activeit.cl>
2024-08-13 16:50:08 -0400

    Added CICD Dashboard and Build Success and Failure widget

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Duration} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codepipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
@@ -10,6 +10,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 
 
 interface ConsumerProps extends StackProps {
@@ -121,8 +122,38 @@ const sourceAction = new codepipelineActions.GitHubSourceAction({
         deploymentConfig: codedeploy.EcsDeploymentConfig.LINEAR_10PERCENT_EVERY_1MINUTES,
         application: ecsCodeDeployApp,
       });
-  
-  
+
+      const buildRate = new cloudwatch.GraphWidget({
+        title: 'Build Successes and Failures',
+        width: 6,
+        height: 6,
+        view: cloudwatch.GraphWidgetView.PIE,
+        left: [
+          new cloudwatch.Metric({
+            namespace: 'AWS/CodeBuild',
+            metricName: 'SucceededBuilds',
+            statistic: 'sum',
+            label: 'Succeeded Builds',
+            period: Duration.days(30),
+          }),
+          new cloudwatch.Metric({
+            namespace: 'AWS/CodeBuild',
+            metricName: 'FailedBuilds',
+            statistic: 'sum',
+            label: 'Failed Builds',
+            period: Duration.days(30),
+          }),
+        ],
+      });
+
+      new cloudwatch.Dashboard(this, 'CICD_Dashboard', {
+        dashboardName: 'CICD_Dashboard',
+        widgets: [
+          [
+            buildRate,
+          ],
+        ],
+      });
 
 pipeline.addStage({
     stageName: 'Source',
